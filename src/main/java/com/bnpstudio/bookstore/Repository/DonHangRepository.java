85340300bd9a25acd55e4c7e97e6e5f722122a60
VuThankBinh <binhthanh.vubc269@gmail.com>
2024-11-11 16:48:41 +0700

    chắc là xong sách r

--- src/main/java/com/bnpstudio/bookstore/Repository/DonHangRepository.java
+++ src/main/java/com/bnpstudio/bookstore/Repository/DonHangRepository.java
@@ -2,6 +2,8 @@ package com.bnpstudio.bookstore.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,10 +15,11 @@ public interface DonHangRepository extends JpaRepository<DonHangEntity,Integer> 
     @SuppressWarnings("null")
     List<DonHangEntity> findAll();
     //get theo trạng thái
-    List<DonHangEntity> findByTrangThai(String trangThai);
+    Page<DonHangEntity> findByTrangThaiIgnoreCase(String trangThai,Pageable pageable );
     //chỉnh sửa trạng thái: xác nhận - hủy - đã gửi - đã nhận
-    boolean updateTrangThai(int id, String trangThai);
+    // DonHangEntity updateTrangThaiDonHangEntity(Integer id, String trangThai);
     //get chi tiết
+
     DonHangEntity findById(int id);
-    //print đơn hàng + vận chuyển
+    //print đơn hàng + vận chuyểnint
 }
--- src/main/java/com/bnpstudio/bookstore/Service/DonHangService.java
+++ src/main/java/com/bnpstudio/bookstore/Service/DonHangService.java
@@ -1,13 +1,53 @@
 package com.bnpstudio.bookstore.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.bnpstudio.bookstore.dto.DonHangDetailDto;
+import com.bnpstudio.bookstore.entity.DonHangEntity;
+import com.bnpstudio.bookstore.exception.NotFoundException;
 import com.bnpstudio.bookstore.repository.DonHangRepository;
 
 @Service
 public class DonHangService {
     @Autowired
     DonHangRepository donHangRepository;
+
+    public List<DonHangDetailDto> getAll() {
+        List<DonHangEntity> donHangs = donHangRepository.findAll();
+        System.out.println("donhangs: " + donHangs);
+        return donHangs.stream()
+                .map(DonHangDetailDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<DonHangDetailDto> getDonHangByTrangThai(Pageable pageable ,String trangThai) {
+        Page<DonHangEntity> donHangs = donHangRepository.findByTrangThaiIgnoreCase(trangThai.trim(),pageable);
+        System.out.println("donhangs: " + donHangs);
+        return donHangs.stream()
+                .map(DonHangDetailDto::new)
+                .collect(Collectors.toList());
+    }
+    public DonHangDetailDto updateTrangThaiDonHang(int id, String trangThai){
+
+        
+        if (trangThai == null || trangThai.trim().isEmpty()) {
+            throw new IllegalArgumentException("Trạng thái đơn hàng không được để trống");
+        }
+        
+        DonHangEntity dh = donHangRepository.findById(id);
+        if(dh == null) {
+            throw new NotFoundException("Không tồn tại đơn hàng có mã là: " + id);
+        }
+        
+        dh.setTrangThai(trangThai);
+        donHangRepository.save(dh);
+        return new DonHangDetailDto(dh);
     
+    }
 }
